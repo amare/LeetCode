3bd773164271f5228d9258404b701bc149deea9f
cuckoo <peisonguo@gmail.com>
2017-03-16 22:16:08 +0800

    add search for a range

---
+++ 034_Search_for_a_Range/README.md
@@ -0,0 +1,12 @@
+
+    Author:            cuckoo
+    Date:              2017/03/16 22:07:01
+    Update:
+    Problem:           Search for a Range
+    Difficulty:        Medium
+    Source:            https://leetcode.com/problems/search-for-a-range
+
+__解决思路__:
+ - 找到第一个与目标值相同的索引
+ - 找到最后一个与目标值相同的索引
+ - 合并
---
+++ 034_Search_for_a_Range/search_for_a_range.h
@@ -0,0 +1,52 @@
+/*
+ Author:            cuckoo
+ Date:              2017/03/16 22:07:01
+ Update:
+ Problem:           Search for a Range
+ Difficulty:        Medium
+ Source:            https://leetcode.com/problems/search-for-a-range
+
+ */
+
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return searchRange_1(nums, target);
+    }
+
+    vector<int> searchRange_1(vector<int>& nums, int target)
+    {
+        vector<int> result = {-1, -1};
+        int low = 0;
+        int high = nums.size() - 1;
+        while(low < high)
+        {
+            int mid = (low + high) / 2;
+            if(nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        if(!nums.empty() && nums[low] == target)
+            result[0] = low;
+
+        low = 0, high = nums.size() - 1;
+        while(low < high)
+        {
+            int mid = (low + high) / 2 + 1;
+            if(nums[mid] < target)
+                low = mid + 1;
+            else if(nums[mid] == target)
+                low = mid;
+            else
+                high = mid - 1;
+        }
+        if(!nums.empty() && nums[high] == target)
+            result[1] = high;
+
+        return result;
+    }
+};
