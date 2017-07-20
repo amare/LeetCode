5f1620ab62991453dc9fe73f305f6e247e5e8b60
cuckoo <peisonguo@gmail.com>
2017-07-20 18:16:48 +0800

    add a new method from LintCode

--- 268_Missing_Number/README.md
+++ 268_Missing_Number/README.md
@@ -1,22 +1,21 @@
 
     Author:            cuckoo
     Date:              2017/03/17 20:35:53
-    Update:
+    Update:            2017/07/20 18:15:16
     Problem:           Missing Number
     Difficulty:        Easy
     Source:            https://leetcode.com/problems/missing-number
 
 __Solution Step__:
-
- - 构造一个标志位数组，其大小为待查找数组元素个数加1，全部初始化为false
- - 遍历待查找数组，对于每个数字，置标志位为true
- - 遍历标志位数组，找到值为false的索引，即缺失数字
+ - First
+     - 构造一个标志位数组，其大小为待查找数组元素个数加1，全部初始化为false
+     - 遍历待查找数组，对于每个数字，置标志位为true
+     - 遍历标志位数组，找到值为false的索引，即缺失数字
+ - Third - [From LintCode](http://www.jiuzhang.com/solution/find-the-missing-number/)
 
 ======
- - [xor](https://discuss.leetcode.com/topic/22313/c-solution-using-bit-manipulation)
-
-> 异或满足交换律和结合律，且自身异或为0。算法中参与异或运算的有
+ - [xor](https://discuss.leetcode.com/topic/22313/c-solution-using-bit-manipulation) - 异或满足交换律和结合律，且自身异或为0。算法中参与异或运算的有
 (0, 1, ..., size - 1), (size) 以及 待查找数组中的所有元素
 
- - [concise xor](https://discuss.leetcode.com/topic/22366/simple-c-codes)
+ - [XOR More Concise](https://discuss.leetcode.com/topic/22366/simple-c-codes)
  - [more](https://discuss.leetcode.com/topic/22305/1-lines-ruby-python-java-c)
--- 268_Missing_Number/missing_number.h
+++ 268_Missing_Number/missing_number.h
@@ -1,7 +1,7 @@
 /*
  Author:            cuckoo
  Date:              2017/03/17 20:35:53
- Update:
+ Update:            2017/07/20 18:15:16
  Problem:           Missing Number
  Difficulty:        Easy
  Source:            https://leetcode.com/problems/missing-number
@@ -14,10 +14,10 @@ using std::vector;
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        return missingNumber_1(nums);
+        return MissingNumberThird(nums);
     }
-
-    int missingNumber_1(vector<int>& nums)
+    
+    int MissingNumberFirst(vector<int>& nums)
     {
         int size = nums.size();
         vector<bool> exist_flag(size + 1, false);
@@ -30,12 +30,13 @@ public:
             if(!exist_flag[i])
                 return i;
         }
-
+        
         return 0;
     }
-
+    
     // xor
-    int missingNumber_2(vector<int>& nums)
+    // all elements except one occur twice, find the one that occurs once
+    int MissingNumberSecond(vector<int>& nums)
     {
         int result = nums.size();
         int i = 0;
@@ -45,7 +46,24 @@ public:
             result ^= i;
             ++i;
         }
-
+        
         return result;
+    }
+    
+    // from LintCode - update at 2017/07/20 18:15:16
+    int MissingNumberThird(vector<int>& nums)
+    {
+        int n = nums.size();
+        for(int i = 0; i < n; ++i)
+        {
+            while(nums[i] != i && nums[i] < n)
+                std::swap(nums[i], nums[nums[i]]);
+        }
+        
+        for(int i = 0; i < nums.size(); ++i)
+            if(nums[i] != i)
+                return i;
+        
+        return n;
     }
 };
